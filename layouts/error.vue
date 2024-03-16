2b61afd05ec5aea2715d4ca2bb9ba11c32de29e9
Aldrich Escudero <aldrich.escudero@gmail.com>
2024-03-16 11:54:13 +0800

    Updated empty and error layouts

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,45 +1,54 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="6" class="text-center">
+          <v-icon size="120" class="mb-4">mdi-alert-octagon</v-icon>
+          <h1 v-if="error.statusCode === 404">
+            {{ pageNotFound }}
+          </h1>
+          <h1 v-else>
+            {{ otherError }}
+          </h1>
+          <v-btn color="red" dark large class="mt-5" :to="'/'">
+            Return to Home Page
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'EmptyLayout',
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ErrorLayout',
   layout: 'empty',
   props: {
-    error: {
-      type: Object,
-      default: null
-    }
+    error: Object as () => ({
+      statusCode: Number,
+    }),
   },
-  data () {
+  data() {
     return {
       pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
+      otherError: 'An error occurred',
+    };
   },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+  head() {
+    const title = this.error && this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
-      title
-    }
-  }
-}
+      title,
+    };
+  },
+});
 </script>
 
 <style scoped>
 h1 {
-  font-size: 20px;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 20px;
 }
 </style>
