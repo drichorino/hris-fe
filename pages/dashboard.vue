6abe5d52a53868c921ed9637477f1b8526a38df4
Aldrich Escudero <aldrich.escudero@gmail.com>
2024-03-17 20:07:16 +0800

    Added dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container fluid> 
+      <v-row justify="center">
+        <v-col cols="12" md="4">
+          <v-card class="pa-6">
+            DASHBOARD
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script lang="ts">
+  import Vue from 'vue';
+  
+  export default Vue.extend({
+    name: 'DashboardPage',
+    components: {
+
+    },
+    data: () => ({
+
+    }),
+  });
+  </script>
