138347f58d3d451d4a75fa6eafdc9c5aa695a27b
Aldrich Escudero <aldrich.escudero@gmail.com>
2024-03-17 20:04:57 +0800

    Updated SimpleForm component

--- components/SimpleForm.vue
+++ components/SimpleForm.vue
@@ -1,63 +1,63 @@
 <template>
-    <v-form>
-      <v-text-field
-        v-for="(field, index) in fields"
-        :key="index"
-        :label="field.label"
-        :prepend-icon="field.icon"
-        :outlined="outlined"
-        v-model="field.model"
-        :type="field.type"
-      ></v-text-field>
-      <v-row justify="center">
-      <v-btn
-        :color="buttonColor"
-        @click="submitForm"
-      >
-        {{ buttonText }}
-      </v-btn>
-    </v-row>
+    <v-form @submit.prevent="submitForm">
+        <v-text-field
+            v-for="(field, index) in fields"
+            :key="index"
+            :label="field.label"
+            :prepend-icon="field.icon"
+            :outlined="outlined"
+            v-model="field.data"
+            :type="field.type"
+            :autocomplete="field.autocomplete"
+        ></v-text-field>
+        <v-row justify="center">
+            <v-btn :color="buttonColor" type="submit">
+                {{ buttonText }}
+            </v-btn>
+        </v-row>
     </v-form>
-  </template>
-  
-  <script lang="ts">
-  import Vue from 'vue';
-  
-  interface Field {
-    label: string;
-    icon: string;
-    model: string;
-    type?: string;
-  }
-  
-  export default Vue.extend({
-    name: "SimpleForm",
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface Field {
+    label: string
+    icon: string
+    data: string
+    type?: string
+    autocomplete?: string
+}
+
+export default Vue.extend({
+    name: 'SimpleForm',
     props: {
-      fields: {
-        type: Array as () => Field[],
-        required: true,
-      },
-      buttonText: {
-        type: String,
-        default: 'Submit',
-      },
-      buttonColor: {
-        type: String,
-        default: 'primary',
-      },
-      outlined: {
-        type: Boolean,
-        default: true,
-      },
+        fields: {
+            type: Array as () => Field[],
+            required: true
+        },
+        buttonText: {
+            type: String,
+            default: 'Submit'
+        },
+        buttonColor: {
+            type: String,
+            default: 'primary'
+        },
+        outlined: {
+            type: Boolean,
+            default: true
+        }
     },
     methods: {
-      submitForm() {
-        // Submission logic here
-        this.fields.forEach((field: Field) => {
-          console.log(`${field.label}:`, field.model);
-        });
-      },
-    },
-  });
-  </script>
-  
+        submitForm(): void {
+            this.$emit('formSubmitted', this.fields)
+
+            // Data Checker
+            // this.fields.forEach((field: Field) => {
+            //     console.log(`${field.label}:`, field.data)
+            // })
+        }
+    }
+})
+</script>
